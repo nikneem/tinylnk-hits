8da7110df43d097128aed2878d864d6d8d5f4b60
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-11-16 16:51:23 +0100

    Changed config to add env vars

--- src/TinyLink.Hits.Functions/Program.cs
+++ src/TinyLink.Hits.Functions/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TinyLink.Hits.TableStorage.ExtensionMethods;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.AddEnvironmentVariables();
+        var config = builder.Build();
+        Console.WriteLine(config["ServiceBus"]);
+    })
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
